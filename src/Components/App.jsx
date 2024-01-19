2ee0998bfd450ef6c4cdad3b31c5a80e0f91b894
culplate <culplate@gmail.com>
2024-01-19 18:08:01 +0100

    added transactions section

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -2,14 +2,17 @@ import './App.css';
 import { useState } from 'react';
 import { Profile } from './Profile/Profile';
 import { FriendList } from './FriendsList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 import userData from '../userData.json';
 import friendsData from '../friends.json';
+import transactions from '../transactions.json';
 
 function App() {
   return (
     <>
       <Profile profileData={userData} />
       <FriendList friendsData={friendsData} />
+      <TransactionHistory items={transactions} />
     </>
   );
 }
